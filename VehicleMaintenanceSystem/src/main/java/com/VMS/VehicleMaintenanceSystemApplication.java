b0d5fc82a42a29e700f0d16ab17a3445057ed0ac
Edon Haziri <edon_haziri@hotmail.com>
2019-12-30 10:03:42 -0500

    ProjectStarter
    Commencing project

---
+++ VehicleMaintenanceSystem/src/main/java/com/VMS/VehicleMaintenanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.VMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleMaintenanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleMaintenanceSystemApplication.class, args);
+	}
+
+}
---
+++ VehicleMaintenanceSystem/src/test/java/com/VMS/VehicleMaintenanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleMaintenanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
